8f0195944ec286ddd8a16fffa92c0096b2748179
BrunoSan <Bruno-kk@hotmail.com>
2021-09-29 15:36:06 -0300

    # controller de terrórios adicionado

--- pokemons/views.py
+++ pokemons/views.py
@@ -18,12 +18,19 @@ def lista_de_pokemon(request):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-  
+@api_view(['GET','POST'])  
 def lista_de_territorio(request):
     if request.method=='GET':
         territorios =Territorios.objects.all()
         serializer = TerritorioSerializado(territorios)
-        return Response(serializer.data)
+        return Response(serializer.data) 
+
+    elif request.method=='POST':
+        serializer=TerritorioSerializado(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)   
 
 
 
