a6593564131a5df89d4aae9fc170ac8f15e1996b
BrunoSan <Bruno-kk@hotmail.com>
2021-10-03 15:51:29 -0300

    # controller de ginasios adicionado

--- myApi/urls.py
+++ myApi/urls.py
@@ -4,7 +4,7 @@ from django.db.models import base
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from pokemons.views import lista_de_pokemon, lista_de_territorio,lista_de_treinador
+from pokemons.views import ginasios, lista_de_pokemon, lista_de_territorio,lista_de_treinador,login_treinador
 from rest_framework import routers
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
@@ -20,6 +20,8 @@ urlpatterns = [
     path('pokemons/',lista_de_pokemon),
     path('territorios/',lista_de_territorio),
     path('treinadores/',lista_de_treinador),
+    path('login-treinador/',login_treinador),
+    path('ginasios/',ginasios),
     path('api/token',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh',TokenRefreshView.as_view(),name='token_refresh')
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- pokemons/views.py
+++ pokemons/views.py
@@ -2,8 +2,18 @@ from rest_framework import serializers, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from pokemons.models.models import Pokemon,Territorios,Treinador
-from pokemons.api.serializers import PokemonSerializado,TerritorioSerializado,treinadorSerializado
+from pokemons.models.models import Ginasios, Pokemon,Territorios,Treinador
+from pokemons.api.serializers import GinasioSerializados, PokemonSerializado,TerritorioSerializado,treinadorSerializado
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def obter_tokens_para_treinador(user):
+    refresh =RefreshToken.for_user(user)
+
+    return{
+        'refresh':str(refresh),
+        'access': str(refresh.access_token)
+    }
 
 
 @api_view(['GET','POST'])
@@ -47,6 +57,29 @@ def lista_de_treinador(request):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET','POST'])
+def ginasios(request):
+    if request.method=='GET':
+        ginasio =Ginasios.objects.all()
+        serializer =GinasioSerializados(ginasio,many=True)
+        return Response(serializer.data)
+    elif request.method=='POST':
+        serializer =GinasioSerializados(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(['POST'])
+def login_treinador(request):
+    treinador = Treinador.objects.filter(nome=request.data)
+    serializer =treinadorSerializado(data=request.data)
+    if serializer == treinador:
+        token =obter_tokens_para_treinador(treinador)
+        return Response(serializer.data,token)
 
 
 
